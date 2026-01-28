d7cadd4787872d0faf2034512b354d38c3b9cf2f
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2026-01-28 14:23:23 +0000

    updates push format

--- src/api/deployments/controllers/push-deployments.js
+++ src/api/deployments/controllers/push-deployments.js
@@ -4,7 +4,8 @@ import { deploymentsForEnvSchema } from '../schemas/deployments-for-env-schema.j
 import { updateDeploymentEnvironment } from '../database/update-deployment-environment.js'
 
 /**
- * Receives per-environment deployment updates pushed from an external source
+ * Receives per-environment deployment updates pushed from an external source.
+ * Expects { environment: 'dev', version: [{name: 'foo', version: '1.2.3'}] }
  */
 const pushDeploymentsController = {
   options: {
@@ -16,7 +17,7 @@ const pushDeploymentsController = {
   handler: async (request, h) => {
     const payload = request?.payload
     const environment = payload.environment
-    const deployments = payload.deployments
+    const deployments = payload.versions
 
     const result = await updateDeploymentEnvironment(
       request,
