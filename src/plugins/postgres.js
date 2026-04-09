b0348966eff1031967bc9a7a59f314458fdac833
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2026-04-09 11:08:43 +0100

    some tweaks to reduce pg connection latency

--- src/plugins/postgres.js
+++ src/plugins/postgres.js
@@ -10,20 +10,62 @@ import Joi from 'joi'
  * @param {{ host: string, port: number, user: string, region: string, useIAM: boolean, localPassword: string|null }} options
  */
 function createPasswordProvider(options) {
-  if (options.useIAM) {
-    return async () => {
-      const signer = new Signer({
-        hostname: options.host,
-        port: options.port,
-        username: options.user,
-        credentials: fromNodeProviderChain(),
-        region: options.region
-      })
-      return await signer.getAuthToken()
-    }
+  if (!options.useIAM) {
+    return () => options.localPassword
   }
 
-  return () => options.localPassword
+  const credentials = fromNodeProviderChain()
+  const signer = new Signer({
+    hostname: options.host,
+    port: options.port,
+    username: options.user,
+    credentials,
+    region: options.region
+  })
+
+  return async () => signer.getAuthToken()
+}
+
+/**
+ * Ensures the connection pool has at least one active connection.
+ * @param pool
+ * @param count
+ * @return {Promise<void>}
+ */
+async function preWarm(pool, count) {
+  const clients = await Promise.all(
+    Array.from({ length: count }, async () => {
+      const client = await pool.connect()
+      try {
+        await client.query('SELECT 1')
+        return client
+      } catch (err) {
+        client.release(true)
+        throw err
+      }
+    })
+  )
+
+  clients.forEach((client) => client.release())
+}
+
+/**
+ * Keep at least one connection active
+ * @param pool
+ * @param intervalMs
+ * @param logger
+ * @return {number|null}
+ */
+function startKeepWarm(pool, intervalMs, logger) {
+  if (!intervalMs) return null
+
+  return setInterval(async () => {
+    try {
+      await pool.query('SELECT 1')
+    } catch (err) {
+      logger?.error(err)
+    }
+  }, intervalMs)
 }
 
 export const postgres = {
@@ -48,20 +90,30 @@ export const postgres = {
         host: value.host,
         port: value.port,
         database: value.database,
-        maxLifetimeSeconds: 60 * 12, // must be less than 15 minutes
+        connectionTimeoutMillis: value.pool.connectionTimeoutMillis,
+        max: value.pool.max,
+        min: value.pool.min,
         ssl: value.ssl,
         ...(value.pool ?? {})
       }
 
       const pool = new Pool(poolConfig)
+      pool.on('error', (err) => {
+        server.logger.error(err)
+      })
 
       // Test the connection
-      await pool.query('SELECT 1')
+      await preWarm(pool, poolConfig.min)
+
+      const keepWarmTimer = startKeepWarm(pool, 60 * 1000, server.logger)
 
       server.decorate('server', 'pg', pool)
       server.decorate('request', 'pg', pool)
 
       server.events.on('stop', async () => {
+        if (keepWarmTimer) {
+          clearInterval(keepWarmTimer)
+        }
         await pool.end()
       })
     }
@@ -79,9 +131,12 @@ const optionsSchema = Joi.object({
   localPassword: Joi.string()
     .min(0)
     .empty(true)
-    .optional()
+    .when('useIAM', {
+      is: false,
+      then: Joi.required(),
+      otherwise: Joi.optional()
+    })
     .description('Static password used when useIAM is set to false'),
-
   region: Joi.string()
     .when('useIAM', {
       is: true,
@@ -92,7 +147,6 @@ const optionsSchema = Joi.object({
   ssl: Joi.boolean().optional(),
   pool: Joi.object({
     connectionTimeoutMillis: Joi.number().optional(),
-    idleTimeoutMillis: Joi.number().optional(),
     max: Joi.number().optional(),
     min: Joi.number().optional(),
     allowExitOnIdle: Joi.boolean().optional()
